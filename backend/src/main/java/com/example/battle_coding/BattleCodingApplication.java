538b27579ec4e1e713b437bdd005f634ff63bcb5
Seol-Munhyeok <abc1234568@naver.com>
2025-03-13 13:37:31 +0900

    초기 프로젝트 설정

---
+++ backend/src/main/java/com/example/battle_coding/BattleCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.battle_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleCodingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/battle_coding/BattleCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.battle_coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleCodingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
