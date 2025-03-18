2a9deb41573d1a7d44ba9282bdc833eadeb10b0a
Seol-Munhyeok <abc1234568@naver.com>
2025-03-18 15:14:09 +0900

    feat:  User 레포지토리 추가
    local 로그인을 위한 findByEmail, OAuth 로그인을 위한 findByProviderId 메서드 추가

--- backend/src/main/java/com/example/battle_coding/entity/User.java
+++ backend/src/main/java/com/example/battle_coding/entity/User.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Builder
 @Table(name = "users")
-public class user {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
