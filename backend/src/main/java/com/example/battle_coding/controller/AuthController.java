335b672d96246d4cfcfb2242e7a8933d59f97688
Seol-Munhyeok <abc1234568@naver.com>
2025-03-18 20:35:10 +0900

    feat: 회원가입 및 로그인 관련 api 설계
    나중에 코드 문서화, db연동 테스트 해야한다.

---
+++ backend/src/main/java/com/example/battle_coding/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.battle_coding.controller;
+
+import com.example.battle_coding.dto.LoginRequestDto;
+import com.example.battle_coding.dto.SignupRequestDto;
+import com.example.battle_coding.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody SignupRequestDto request) {
+        return ResponseEntity.ok(authService.signup(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
